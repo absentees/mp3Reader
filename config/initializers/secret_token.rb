5debc5383aab2ef5553a6a21ff45d191283786db
Scott <djlethal109@gmail.com>
2011-05-17 19:24:13 +1000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mp3Reader::Application.config.secret_token = 'e9a367730b9fb46768badc504ca4007563a149aa765acb942fe155ad0d3180e7383934fbcbc59e2088913222f3d38becce4f70027b56f6f7040c2bb082c4a703'
