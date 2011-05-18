d3bb6aaca877a98a15173daa354752d425d50c7b
Scott <djlethal109@gmail.com>
2011-05-18 23:07:56 +1000

    updated README add user id column and relations to website (may need to remove later).

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,2 +1,9 @@
 class Website < ActiveRecord::Base
+  attr_accessible :websiteUrl
+  belongs_to :user
+
+  validates :user_id, :presence => true
+
+  default_scope :order => "websites.created_at DESC"
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110517095935) do
+ActiveRecord::Schema.define(:version => 20110518070059) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(:version => 20110517095935) do
     t.string   "feedUrl"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
 end
