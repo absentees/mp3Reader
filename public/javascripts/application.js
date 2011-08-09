596d975e11c8e364a948e721d7d2d876d6a50952
absentees <djlethal109@gmail.com>
2011-08-09 22:11:58 +1000

    ajax now working for sorting, searching, pagination

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,15 @@
 $(function() {
-    $("#feed_table th a").live("click", function(){
+    $("#feed_table th a, #feed_table .pagination a").live("click", function(){
         $.getScript(this.href);
         return false;
-    })
+    });
+    $("#feed_table_search submit").keyup(function(){
+       $.get(this.action, $(this).serialize(), null, "script");
+        return false;
+    });
 });
+
+//$("#feed_table_search input").keyup(function(){
+//       $.get($("#feed_table_search").attr("action"), $("#feed_table_search").serialize(), null, "script");
+//        return false;
+//    });
